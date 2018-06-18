6b2907c71e0b611b3dabac33432b2b227c09eada
Philipp <p.napiralla13@googlemail.com>
2018-06-18 16:30:28 +0200

    neue Datei:     EVENTBUILDER.cxx 	neue Datei:     EVENTBUILDER.h 	geändert:       PLASTIC_Data_Stream.h

---
+++ EVENTBUILDER.h
@@ -0,0 +1,19 @@
+#ifndef EVENTBUILDER_H
+#define EVENTBUILDER_H
+
+class EVENTBUILDER{
+
+private:
+
+
+
+public:
+    EVENTBUILDER();
+    ~EVENTBUILDER();
+    
+
+};
+
+
+
+#endif
--- PLASTIC_Data_Stream.h
+++ PLASTIC_Data_Stream.h
@@ -11,10 +11,10 @@ private:
     
     ULong64_t WR_T;
 
-    double E[36];
-    double T[36];
+    double E[100];
+    double T[100];
 
-    int hitID[36];
+    int hitID[100];
     int am_hits;
 
 public:
