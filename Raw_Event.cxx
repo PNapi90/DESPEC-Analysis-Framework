0125d60c4f3bb92ef6af52e349566c1f4bc2b12e
Philipp <p.napiralla13@googlemail.com>
2018-07-01 16:06:05 +0200

    changes

--- Raw_Event.cxx
+++ Raw_Event.cxx
@@ -47,16 +47,16 @@ void Raw_Event::set_DATA_FATIMA(int FAT_FIRED,int TDC_FIRED,double* Ql,double* Q
 	ch51 = false;
 
 	//remaining tdcs
-	for(int i = 0;i < TDC_FIRED;++i){
+	for(int i = FAT_FIRED;i < TDC_FIRED;++i){
 		active_det = det_ids_TDC[i];
-		if(!ch51 && active_det == 51 && position != -5) ch51 = true;
+		//if(!ch51 && active_det == 51 && position != -5) ch51 = true;
 		if(!used_for_QDC[active_det]){
-			Det_Nums[i+FAT_FIRED] = det_ids_TDC[active_det];
-			TDC_timestamp[i+FAT_FIRED] = TDC[active_det];
-			if(active_det >= 50) cout << TDC_timestamp[i+FAT_FIRED] << " " << active_det << " " << TDC_timestamp[0] << endl;
-			if(active_det == 51 && position != -5){
+			Det_Nums[i] = det_ids_TDC[active_det];
+			TDC_timestamp[i] = TDC[active_det];
+			if(active_det >= 50) cout << TDC_timestamp[i] << " " << active_det << " " << TDC_timestamp[0] << endl;
+			if(active_det == 51 && position != -5 && !ch51){
 				ch51 = true;
-				time_difference = ((double) TDC_timestamp[i+FAT_FIRED]) - ((double) TDC_timestamp[0]);
+				time_difference = ((double) TDC_timestamp[i]) - ((double) TDC_timestamp[0]);
 			}
 		}
 	}
