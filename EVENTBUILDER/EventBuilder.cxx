c6d541f27efd3c077a4f3471129fc973fbe887d1
PhilippNapiralla <p.napiralla13@googlemail.com>
2018-07-11 15:38:09 +0200

    Time EventBuilder

---
+++ EVENTBUILDER/EventBuilder.cxx
@@ -0,0 +1,20 @@
+#ifndef EVENTBUILDER_H
+#define EVENTBUILDER_H
+
+class EventBuilder{
+
+public:
+	
+	virtual ~EventBuilder() = 0;
+
+	virtual void set_Event(Raw_Event*) = 0;
+
+	virtual void 
+
+
+
+
+	
+};
+
+#endif
