8383f296f4dfcc423192c6e95d6620e6ecfceab0
Philipp <p.napiralla13@googlemail.com>
2018-07-27 14:02:46 +0200

    Small Galileo change 3

--- GALILEO_Time_Calibration.cxx
+++ GALILEO_Time_Calibration.cxx
@@ -35,12 +35,13 @@ void GALILEO_Time_Calibration::load_Calibration_File(){
     int det_id;
 
     while(getline(calib_file,line,'\n')){
+        cout << line << endl;
         if(line[0] == '#') continue;
         sscanf(line.c_str(),format,&det_id,&tmp_sum_coeffs,&tmp_chan_coeffs);
 
         calib_coeffs_sum[det_id] = tmp_sum_coeffs;
         calib_coeffs_channels[det_id] = tmp_chan_coeffs;
-	
+        cout << det_id << " " << tmp_sum_coeffs << " " << tmp_chan_coeffs << endl;
     }
 }
 
