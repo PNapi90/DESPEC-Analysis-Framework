9c755a72a54129907dad77f9601b041f104424c0
Philipp <p.napiralla13@googlemail.com>
2018-07-30 09:40:45 +0200

    Fat Evt changes

--- FATIMA_Event.cxx
+++ FATIMA_Event.cxx
@@ -16,7 +16,7 @@ FATIMA_Event::~FATIMA_Event(){}
 
 inline void FATIMA_Event::set_DATA(Raw_Event* RAW){
     //something
-    FAT_FIRED = RAW->get_FATIMA_am_Fired();
+    //FAT_FIRED = RAW->get_FATIMA_am_Fired();
 
     return;
 }
