f97a0ab52415b6110c60bdf4ebcf2a5849bcdec9
PNapi90a <p.napiralla13@googlemail.com>
2018-09-09 18:03:17 +0200

    change

--- AIDA_Decay_Event_Store.cxx
+++ AIDA_Decay_Event_Store.cxx
@@ -49,6 +49,7 @@ void AIDA_Decay_Event_Store::Store_Decay_Event(ADCDataItem* Decay_Item){
     x_coord[amount] = Decay_Item->GetRealChannelID();
     Time[amount] = Decay_Item->GetTimestamp();
 
+    cout << "AMOUNT " << amount << endl;
     Decay_Item->Print_Event();
 
     amount++;
