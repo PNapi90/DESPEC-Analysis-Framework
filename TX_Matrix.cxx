52330245f68e6e561d02397055d8929fc98b228f
Philipp <p.napiralla13@googlemail.com>
2018-09-11 15:05:56 +0200

    Change

--- TX_Matrix.cxx
+++ TX_Matrix.cxx
@@ -377,6 +377,7 @@ void TX_Matrix::Thread_X(int thr_num){
 
         MUTEX.unlock();
         //===========================================================
+        c_counter = 0;
         cout << "next " << i+1 << " " << row_start << " " << data_points_per_thr_tmp+row_start << endl;
     }
 }
