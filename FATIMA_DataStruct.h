cc144690bf6273b9d2a4543a55f0d6c81eb7c53f
PNapi90a <p.napiralla13@googlemail.com>
2018-12-11 14:53:15 +0100

    Change

--- FATIMA_DataStruct.h
+++ FATIMA_DataStruct.h
@@ -1,8 +1,11 @@
 #ifndef FATIMA_DATA_STRUCT_H
 #define FATIMA_DATA_STRUCT_H
 
-
+#include <iostream>
 #include <vector>
+
+typedef unsigned long ULong;
+typedef unsigned long long ULong64_t;
 
 struct FATIMA_DataStruct{
 
@@ -25,11 +28,38 @@ struct FATIMA_DataStruct{
     std::vector<int>    FAT_TDC_id;
     std::vector<double> FAT_TDC_timestamp;//tdc time raw
 
-    FATIMA_DataStruct() : FAT_DET_FIRED(0) , FAT_id(100,0),FAT_E(100,0),FAT_ratio(100,0),FAT_t(100,0),FAT_t_qdc(100,0),
-                          FAT_QDCs_FIRED(0) , FAT_QDC_id(100,0),FAT_QLong(100,0),FAT_QLong_Raw(100,0),FAT_QShort_Raw(100,0),
+    FATIMA_DataStruct() : FAT_DET_FIRED(0),FAT_id(100,0),FAT_E(100,0),FAT_ratio(100,0),FAT_t(100,0),FAT_t_qdc(100,0),
+                          FAT_QDCs_FIRED(0),FAT_QDC_id(100,0),FAT_QLong(100,0),FAT_QLong_Raw(100,0),FAT_QShort_Raw(100,0),
                           FAT_QDC_t_coarse(100,0),FAT_QDC_t_fine(100,0),FAT_TDCs_FIRED(0),FAT_TDC_id(100,0),FAT_TDC_timestamp(100,0)
     {}
 
+    SetDATA_Directly(int _FAT_DET_FIRED,std::vector<int> &_FAT_id,std::vector<double> &_FAT_E,std::vector<double> &_FAT_ratio,
+                     std::vector<double> &_FAT_t,std::vector<double> &_FAT_t_qdc,int _FAT_QDCs_FIRED,std::vector<int> &_FAT_QDC_id,
+                     std::vector<double> &_FAT_QLong,std::vector<double> &_FAT_QLong_Raw,std::vector<double> &_FAT_QShort_Raw,
+                     std::vector<ULong> &_FAT_QDC_t_coarse,std::vector<double> &_FAT_QDC_t_fine,std::vector<double> &_FAT_TDCs_FIRED,
+                     std::vector<int> &_FAT_TDC_id,std::vector<double> &_FAT_TDC_timestamp)
+    {
+        FAT_DET_FIRED = _FAT_DET_FIRED;
+        FAT_TDCs_FIRED = _FAT_TDCs_FIRED;
+        FAT_QDCs_FIRED = _FAT_QDCs_FIRED;
+        
+        for(int i = 0;i < 100;++i){
+            FAT_id[i] = _FAT_id[i];
+            FAT_E[i] = _FAT_E[i];
+            FAT_ratio[i] = _FAT_ratio[i];
+            FAT_t[i] = _FAT_t[i];
+            FAT_t_qdc[i] = _FAT_t_qdc[i];
+            
+            FAT_QDC_id[i] = _FAT_QDC_id[i];
+            FAT_QLong[i] = _FAT_QLong[i];
+            FAT_QLong_Raw[i] = _FAT_QLong[i];
+            FAT_QShort_Raw[i] = _FAT_QShort_Raw[i];
+            FAT_QDC_t_coarse[i] = _FAT_QDC_t_coarse[i];
+            FAT_QDC_t_fine[i] = _FAT_QDC_t_fine[i];
+            FAT_TDC_id[i] = _FAT_TDC_id[i]; 
+            FAT_TDC_timestamp[i] = _FAT_TDC_timestamp[i];
+        }
+    }
 
 
     SetDATA(int QDC_FIRED,int TDC_FIRED,std::vector<double> &Ql_Raw,std::vector<double> &Qs_Raw,
@@ -42,7 +72,7 @@ struct FATIMA_DataStruct{
         int dets_fired = 0;
         for (int i=0; i<QDC_FIRED; i++) {
             FAT_QDC_id[i] = det_ids_QDC[i];
-            if (det_ids_QDC[i] == 35) cout<<"I am in QDC"<<endl;
+            if (det_ids_QDC[i] == 35) std::cout<<"I am in QDC"<<std::endl;
             FAT_QLong[i]  = Ql[i];
             FAT_QLong_Raw[i]  = Ql_Raw[i];
             FAT_QShort_Raw[i] = Qs_Raw[i];
@@ -51,7 +81,7 @@ struct FATIMA_DataStruct{
             for (int j=0; j<TDC_FIRED; j++) {
                 if (det_ids_QDC[i] == det_ids_TDC[j]) {
                     FAT_id[dets_fired] = det_ids_QDC[i];
-                    if (det_ids_TDC[j] == 35) cout<<"I am in TDC"<<endl;
+                    if (det_ids_TDC[j] == 35) std::cout<<"I am in TDC"<<std::endl;
 
                     FAT_E[dets_fired] = Ql[i];
                     FAT_ratio[dets_fired] = (double) Qs_Raw[i]/Ql_Raw[i];
--- FATIMA_Event.cxx
+++ FATIMA_Event.cxx
@@ -18,7 +18,7 @@ inline void FATIMA_Event::set_DATA(Raw_Event* RAW){
 
     FATIMA_DataStruct* P_Tmp = RAW->PassFATIMA();
     //set event data
-    DATA.SetDATA(P_Tmp->FAT_DET_FIRED,P_Tmp->FAT_id,P_Tmp->FAT_E,P_Tmp->FAT_ratio,P_Tmp->FAT_t,
+    DATA.SetDATA_Directly(P_Tmp->FAT_DET_FIRED,P_Tmp->FAT_id,P_Tmp->FAT_E,P_Tmp->FAT_ratio,P_Tmp->FAT_t,
                  P_Tmp->FAT_t_qdc,P_Tmp->FAT_QDCs_FIRED,P_Tmp->FAT_QDC_id,P_Tmp->FAT_QLong,
                  P_Tmp->FAT_QLong_Raw,P_Tmp->FAT_QShort_Raw,P_Tmp->FAT_QDC_t_coarse,
                  P_Tmp->FAT_QDC_t_fine,P_Tmp->FAT_TDCs_FIRED,P_Tmp->FAT_TDC_id,P_Tmp->FAT_TDC_timestamp);
