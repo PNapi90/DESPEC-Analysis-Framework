6bc0d06b285e30b86573031ad45f8fe64e19b634
PhilippNapiralla <p.napiralla13@googlemail.com>
2018-12-13 10:45:45 +0100

    .root added in Root_Trees/EventBuilder/

--- PL_FAT_EventProcessor.cxx
+++ PL_FAT_EventProcessor.cxx
@@ -5,7 +5,7 @@
 PL_FAT_EventProcessor::PL_FAT_EventProcessor() : PLASTIC_Passed(false) , 
                                                  FATIMA_Passed(false)
 {
-    FILE = new TFile("PL_FAT_EventBuild.root","RECREATE");
+    FILE = new TFile("Root_Trees/EventBuilder/PL_FAT_EventBuild.root","RECREATE");
 
     char Name[50];
 
@@ -26,10 +26,11 @@ PL_FAT_EventProcessor::PL_FAT_EventProcessor() : PLASTIC_Passed(false) ,
         for(int j = 0;j < MAX_PL;++j){
             fileName = folder + "_PL_" + std::to_string(j) + "_TT";
             fileName2 = folder + "_PL_" + std::to_string(j) + "_EvsE";
-            TDiff[i][j] = nullptr;
-            //TDiff[i][j] = new TH1F(fileName.c_str(),fileName.c_str(),2500,-4000,1000);
+
+            TDiff[i][j] = new TH1F(fileName.c_str(),fileName.c_str(),2500,-4000,1000);
             EvE[i][j] = new TH2F(fileName2.c_str(),fileName2.c_str(),500,0,2000,500,0,2000);
-            //FILE->Add(TDiff[i][j]);
+            
+            FILE->Add(TDiff[i][j]);
             FILE->Add(EvE[i][j]);
         }
         FILE->cd();
@@ -131,7 +132,7 @@ void PL_FAT_EventProcessor::ProcessEvent(){
 			
 			if(abs(FAT_Ch) <= MAX_FAT && abs(PL_Ch) <= MAX_PL){
                 EvE[FAT_Ch][PL_Ch]->Fill(EFat,EPL);
-                //TDiff[FAT_Ch][PL_Ch]->Fill(Diff_FAT_PL);
+                TDiff[FAT_Ch][PL_Ch]->Fill(Diff_FAT_PL);
             }
             //TWalk_MAT[FAT_Ch][PL_Ch]->Fill(ToT,Diff_FAT_PL);
         }
